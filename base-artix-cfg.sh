cd8f546d0b5173bc3b17949e7de2b4cf14cab582
InquireWithin <94314593+InquireWithin@users.noreply.github.com>
2022-05-13 14:14:34 -0400

    patch

--- base-artix-cfg.sh
+++ base-artix-cfg.sh
@@ -8,10 +8,9 @@
 #sudo su
 
 
+#provides verbosity when errors inevitably occur
+set -x
 
-#01-06/5/22
-#installed the OS and zsh
-#12/5/22 script install zsh
 sudo pacman -Syu zsh wget man gedit git artix-archlinux-support xf86-video-intel nvidia nvidia-settings hwinfo htop
 chsh -s /bin/zsh
 #default editor as vim, persistently
@@ -21,13 +20,12 @@ sudo chmod 777 /etc/pacman.conf
 sudo echo "[extra]\nInclude = /etc/pacman.d/mirrorlist-arch\n\n[community]\nInclude=/etc/pacman.d/mirrorlist-arch\n\n[multilib]\nInclude=/etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf
 sudo chmod 644 /etc/pacman.conf
 pacman-key --populate archlinux
-#12/5/22 
 #yay installation b/c eventually I'll need an AUR exclusive package
 cd /opt/
-sudo git clone https://aur.archlinux.org/yay-git.git && cd yay-git && makepkg -si
+#this wont work in root or root account
+sudo git clone https://aur.archlinux.org/yay-git.git && chmod 777 -r yay-git && cd yay-git && makepkg -si
 cd $HOME
 
-# 12/5/22 spent ages digging the nvidia rabbithole
 #install nvidia drivers for NV110/GMXXX series or higher (checked via nvidia driver download page)
 # proprietary drivers, using for xorg (dependency of xfce)
 # default linux kernel
@@ -36,14 +34,14 @@ chmod 777 /etc/X11/xorg.conf.d/
 # ^ Leaving this as is due to the fact it needs to be read and accessed by the user. Don't know if it needs to write but not worth the gamble of breaking the whole config.
 sudo touch /etc/X11/xorg.conf.d/20-intel.conf
 sudo chmod 777 /etc/X11/xorg.conf.d/20-intel.conf
-echo Section "Device"\n\tIdentifier "Intel Graphics"\n\tDriver "Intel"\nEndSection >> /etc/X11/xorg.conf.d/20-intel.conf
+echo "Section \"Device\"\n\tIdentifier \"Intel Graphics\"\n\tDriver \"Intel\"\nEndSection" >> /etc/X11/xorg.conf.d/20-intel.conf
 #32 bit app support (multilib req)
 #sudo pacman -S lib32-nvidia-utils
 sudo mkdir /etc/pacman.d/hooks
 sudo chown $USER:$USER /etc/pacman.d/hooks
 sudo touch /etc/pacman.d/hooks/nvidia.hook
 #avoids no upgrade of initramfs after upgrading nvidia drivers, also prevents mkinitcpio from running more than once
-sudo echo [Trigger]\nOperation=Install\nOperation=Remove\nType=Package\nTarget=nvidia\nTarget=linux\n[Action]\nDepends=mkinitcpio\nWhen=PostTransaction\nNeedsTargets\nExec=/bin/sh -c 'while read -r trg; do case $trg in linux) exit 0; esac ; done; /usr/bin/mkinitcpio -P' > /etc/pacman.d/hooks/nvidia.hook 
+sudo echo "[Trigger]\nOperation=Install\nOperation=Remove\nType=Package\nTarget=nvidia\nTarget=linux\n[Action]\nDepends=mkinitcpio\nWhen=PostTransaction\nNeedsTargets\nExec=/bin/sh -c 'while read -r trg; do case $trg in linux) exit 0; esac ; done; /usr/bin/mkinitcpio -P'" > /etc/pacman.d/hooks/nvidia.hook 
 
 sudo chown $USER:$USER -r /etc/pacman.d/hooks #chmod 777 worked for me for all instances of this the first time
 sudo chown $USER:$USER -r /etc/X11
@@ -51,23 +49,23 @@ sudo nvidia-xconfig
 
 #two monitor support
 #this is VERY SPECIFIC, this command syntax I use here assumes you have 2 monitors: one is HDMI connected via HDMI-1 and it is on the right (user facing) of
-#a monitor connected by DVI-I-1
+#a monitor connected by DVI-I-1. For my setup, the resolution defaults to 1920x1080, though I believe --mode can configure it (See "Multihead" on Arch Wiki)
 xrandr --output DVI-I-1 --auto --output HDMI-0 --auto --right-of DVI-I-1
 #change primary monitor via display settings GUI of xfce4 (click on applications on the left of the top panel and find "Settings Manager" or "Display")
-#11/5/22
+
 touch ~/.xinitrc
-#echo [[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] >> ~/.xinitrc
+echo "[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]]" >> ~/.xinitrc
 sudo pacman -S pulseaudio-alsa lib32-libpulse lib32-alsa-plugins flatpak
-rm ~/.asoundrc # its not supposed to exist
+[[ -f $HOME/.asoundrc ]] && rm $HOME/.asoundrc # its not supposed to exist
 cd $HOME
+sudo chmod u+r /etc/pulse/daemon.conf #just in case this permission isn't present
+#copy settings from the system's configuration into a user-specific config (changes made in the file inside home (~) will only affect the current user and are not system wide
 cp /etc/pulse/daemon.conf ~/.pulse/daemon.conf
 sudo echo flat-volumes=no >> ~/.pulse/daemon.conf
-killall xfce4-panel #restart panel
-xfce4-panel &
-disown
+#killall xfce4-panel #restart panel
+#xfce4-panel &
+#disown
 #yay gets invalidated if ran as root user
 yay -S noise-suppression-for-voice
 
-#12/5/22
-sudo pacman -S git
 
