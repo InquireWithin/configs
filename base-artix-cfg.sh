d46473a935b573cfd2d7af35098453a15d769953
InquireWithin <juicebin@protonmail.com>
2022-06-30 14:37:24 -0400

    more sensible syntax & better structure

--- base-artix-cfg.sh
+++ base-artix-cfg.sh
@@ -24,24 +24,27 @@ USEZSH="TRUE" #flags whether zsh should be installed and used over bash as a def
 PKGFILE= #filepath to manually download packages from
 #either get these basic utilities or import from file
 
-if [ -n ${PKGFILE+x} ]; then sudo pacman -S --noconfirm - < $PKGFILE ;else sudo pacman -Syu --noconfirm curl vim wget man gedit git artix-archlinux-support hwinfo htop unzip gnu-free-fonts ttf-liberation traceroute pulseaudio-alsa lib32-libpulse lib32-alsa-plugins
+#this script currently only supports intel cpu's. it can be ran w/o an nvidia gpu but no graphical config will be made. It can be ran w/o setting usexfce4 TRUE, but no DE config will be made.
+
+if [ -n ${PKGFILE+x} ]; then sudo pacman -S --noconfirm - < $PKGFILE 
+#recommended but optional packages
+#sudo pacman -Syu --noconfirm  wget gedit hwinfo htop unzip gnu-free-fonts ttf-liberation traceroute 
+!([[ -d /bin/curl ]]) && sudo pacman -S --noconfirm curl man vim artix-archlinux-support git pulseaudio-alsa lib32-libpulse lib32-alsa-plugins
 if [ $USEZSH=="TRUE" ] then
 sudo pacman -S zsh
 chsh -s /bin/zsh
 #zshrc
 yay -S zsh-syntax-highlighting #replace this with the git process so its automated
-!([[ -d /bin/curl ]]) && sudo pacman -S --noconfirm curl
 curl https://github.com/InquireWithin/configs/tree/main/dotfiles/.zshrc >> ~/.zshrc
 fi
 if [ $USEXFCE4=="TRUE" ] then
 sudo pacman -S xfce4 xfce4-goodies
 fi
 #default editor as vim, persistently
-echo "export EDITOR=/bin/vim" >> /etc/profile
+echo "export $EDITOR=/bin/vim" >> /etc/profile
 sudo chmod 777 /etc/pacman.conf
 #put compatibility for extra, mutilib, and community here (see software.txt)
 sudo echo "[extra]\nInclude = /etc/pacman.d/mirrorlist-arch\n\n[community]\nInclude=/etc/pacman.d/mirrorlist-arch\n\n[multilib]\nInclude=/etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf
-sudo chmod 644 /etc/pacman.conf
 pacman-key --populate archlinux
 #yay installation b/c eventually I'll need an AUR exclusive package
 cd /opt/
@@ -55,7 +58,7 @@ cd $HOME
 # default linux kernel
 if [ $CARDTYPE == "NVIDIA" ] then
 #creating a "just in case" file as the wiki said making a 20-intel.conf is preferred over xorg.conf
-sudo pacman -S xf86-video-intel nvidia nvidia-settings
+sudo pacman -S --noconfirm xf86-video-intel nvidia nvidia-settings
 [[ $USEXFCE4=="TRUE" ]] && chmod 777 /etc/X11/xorg.conf.d/ && sudo touch "/etc/X11/xorg.conf.d/20-intel.conf"
 sudo chmod 777 /etc/X11/xorg.conf.d/20-intel.conf && echo "Section \"Device\"\n\tIdentifier \"Intel Graphics\"\n\tDriver \"Intel\"\nEndSection" >> /etc/X11/xorg.conf.d/20-intel.conf
 # ^ Leaving this as is due to the fact it needs to be read and accessed by the user. Don't know if it needs to write but not worth the gamble of breaking the whole config.
@@ -75,10 +78,7 @@ fi
 #xrandr --output DVI-I-1 --auto --output HDMI-0 --auto --right-of DVI-I-1
 #change primary monitor via display settings GUI of xfce4 (click on applications on the left of the top panel and find "Settings Manager" or "Display")
 
-
-if [ $USEXFCE4 == "TRUE" ] then
-echo "[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]]" >> ~/.xinitrc
-fi
+[[ $USEXFCE4 == "TRUE" ]] && echo "#!/bin/bash\n[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx" >> ~/.xinitrc
 [[ -f $HOME/.asoundrc ]] && rm $HOME/.asoundrc # its not supposed to exist
 cd $HOME
 sudo chmod u+r /etc/pulse/daemon.conf #just in case this permission isn't present
@@ -93,7 +93,8 @@ tabs 4 #tabs should now be set to 4 spaces
 git config --global user.name $GITUSER
 git config --global user.email $GITMAIL
 git config --global core.editor $EDITOR
-git config --global init.defaultBranch master
+#personal preference, I still have repos that use this, they screwed people over when they changed the default.
+#git config --global init.defaultBranch master
 
 #vimrc
 curl https://github.com/InquireWithin/configs/tree/main/dotfiles/.vimrc > ~/.vimrc
@@ -102,7 +103,7 @@ curl https://github.com/InquireWithin/configs/tree/main/dotfiles/.bashrc > ~/.ba
 
 #hosts file domain blocking (decreases the urgency of acquiring an ad-blocker and helps for browsers with limited support for ad-blocking extensions or integration)
 sudo chmod 777 /etc/hosts
-curl https://raw.githubusercontent.com/InquireWithin/resources/main/ublock_adblock_server_filter.txt >> /etc/hosts
+curl https://github.com/InquireWithin/resources/tree/main/LF/LF_extended_telemetry_list.txt >> /etc/hosts
 sudo chmod 644 /etc/hosts
 
 
