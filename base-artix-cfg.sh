55217b09a500821ab04095897e9fdb34ae7ae624
InquireWithin <94314593+InquireWithin@users.noreply.github.com>
2022-07-28 14:34:03 -0400

    see diffs

--- base-artix-cfg.sh
+++ base-artix-cfg.sh
@@ -1,17 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #provides verbosity when errors inevitably occur
 set -x
 
 #USER-SET VARIABLES. SET THESE MANUALLY
 PASSWORD="" #password for the user running the script.
 #manually configure these variables:
-GITUSER="" #username for git config.
-GITMAIL="" #email for git config.
-DEFAULTBRANCHNAME="master"
 USEXFCE4="TRUE" #set up for xfce4 desktop environment.
 USEZSH="TRUE" #flags whether zsh should be installed and used over bash as a default shell.
 PKGFILE="" #filepath of a text file for a list of packages for pacman to install
+USEARCHREPOS="FALSE" #use arch repos such as multilib, extra, and community, requires repo lib32 to already be enabled, and requires mirrorlist-arch, must also be below all artix repos in /etc/pacman.conf. Might also need artix-archlinux-support
 
+#OPTIONAL
+GITUSER="" #username for git config.
+GITMAIL="" #email for git config.
+DEFAULTBRANCHNAME="master"
 
 #NON-MANUALLY SET VARIABLES
 CARDTYPE="" #graphics card manufacturer.
@@ -25,16 +27,16 @@ CARDTYPE="" #graphics card manufacturer.
 #I'm not a fan of wayland (pretty obvious why considering this script is made for nvidia cards), so I assume only X11 is used.
 
 #don't run as root
-[ "$(id -u)" -eq 0 ] &&echo "This script should not be ran as root, as most of it will break. Consider creating or logging into a user account that can use sudo before running."&& exit 1
+[ "$(id -u)" -eq 0 ] && echo "This script should not be ran as root, as most of it will break. Consider creating or logging into a user account that can use sudo before running." && exit 1
 
 #for some ungodly reason there are exactly 7 spaces prior to the string, piping into sed w/ that expression will remove preceding whitespace.
 [[ $(sudo lshw -C display | grep vendor | sed 's/^ *//g') == "vendor: NVIDIA Corporation" ]] && CARDTYPE="NVIDIA"
-#graphics driver config-dependent.
+#graphics driver config-dependent. Fix the pipe into grep, it's bloat.
 [[ $(lspci | grep -ci vga) > 1 ]] && echo "ERROR: Script currently not supported for > 1 graphics card"
-#If the password field is not an empty string, feed it into sudo.
 export HISTIGNORE='*sudo -S*' #done so that pw cant be reaped from shell history.
 #sudo -S would typically work fine, but 'k' is used to continuously refresh the sudo auth cache so the password is never seen in the shell or executed as a command.
 #sudo -Sk allows greater consistency as well, working the same regardless of the user's sudo timeout (iirc it's 5 min by default, some people change it.)
+#If the password field is not an empty string, feed it into sudo.
 [[ ! -n ${PASSWORD+x} ]] && alias sudo="echo "$PASSWORD" | sudo -Sk" #might break commands that require extra input after exec'd (pacman, for example)
 
 #Will install whatever packages are specified in the filepath in $PKGFILE assuming the variable isnt set to an empty string (null)
@@ -42,31 +44,42 @@ export HISTIGNORE='*sudo -S*' #done so that pw cant be reaped from shell history
 #recommended but optional packages
 #sudo pacman -Syu --noconfirm wget gedit hwinfo htop unzip traceroute neomutt neovim shutter btrfs-progs dosfstools dos2unix transmission-cli groff scim
 #essentials
-sudo pacman -Syu --noconfirm curl man vim artix-archlinux-support git pulseaudio pulseaudio-alsa lib32-libpulse lib32-alsa-plugins zathura mpv pavucontrol gnu-free-fonts ttf-liberation
+sudo pacman -Syu --noconfirm curl man vim git pulseaudio pulseaudio-alsa lib32-libpulse lib32-alsa-plugins zathura mpv pavucontrol gnu-free-fonts ttf-liberation
 
 #yay installation b/c eventually I'll need an AUR exclusive package. this wont work in root user
 cd /opt && sudo git clone https://aur.archlinux.org/yay-git.git && sudo chmod 777 -r yay-git && cd yay-git && makepkg -si && cd $HOME
 #yay -S --noconfirm ttf-ms-win11 #if you ever plan to use steam, uncomment
 
-if [ $USEZSH=="TRUE" ] then
+#Ensuring XDG directory specifcation defaults are met. Mostly redundant, however.
+declare -A xdgarr=(["XDG_DATA_HOME"]='$HOME/.local/share' ["XDG_CONFIG_HOME"]='$HOME/.config' ["XDG_CACHE_HOME"]='$HOME/.cache' ["XDG_DATA_DIRS"]='usr/local/share/:/usr/share/' ["XDG_CONFIG_DIRS"]='/etc/xdg')
+for i in "${!xdgarr[@]}"
+    do
+        sudo echo $i="${xdgarr[$i]}" >> /etc/profile
+    done
+
+#zsh
+if [ $USEZSH = "TRUE" ] then
     sudo pacman -S --noconfirm zsh
     chsh -s /bin/zsh
-    yay -S --noconfirm zsh-syntax-highlighting
+    yay -S --noconfirm zsh-syntax-highlighting &
     curl https://github.com/InquireWithin/configs/tree/main/dotfiles/.zshrc >> ~/.zshrc
     source ~/.zshrc
 fi
-[[ $USEXFCE4=="TRUE" ]] && sudo pacman --noconfirm -S xfce4 xfce4-goodies
+[[ $USEXFCE4 = "TRUE" ]] && sudo pacman --noconfirm -S xfce4 xfce4-goodies
 #default editor as vim, persistently, except if nvim exists which would be used instead.
 [[ -f /usr/bin/nvim ]] && sudo echo "EDITOR=/usr/bin/nvim" >> /etc/environment || sudo echo "EDITOR=/usr/bin/vim" >> /etc/environment
 sudo chmod 777 /etc/pacman.conf
-#Integrate compatibility for extra, mutilib, and community repos here 
-sudo echo "[extra]\nInclude = /etc/pacman.d/mirrorlist-arch\n\n[community]\nInclude=/etc/pacman.d/mirrorlist-arch\n\n[multilib]\nInclude=/etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf
-pacman-key --populate archlinux
+echo "[lib32]\nInclude = /etc/pacman.d/mirrorlist\n[universe]\nServer = https://universe.artixlinux.org/$arch\nServer = https://mirror1.artixlinux.org/universe/$arch\nServer = https://mirror.pascalpuffke.de/artix-universe/$arch\nServer = https://artixlinux.qontinuum.space/artixlinux/universe/os/$arch\nServer = https://mirror1.cl.netactuate.com/artix/universe/$arch\nServer = https://ftp.crifo.org/artix-universe/" >> /etc/pacman.conf
+#Integrate compatibility for extra, mutilib, and community repos here, if enabled
+sudo pacman -Sy --noconfirm archlinux-keyring artix-archlinux-support #artix-archlinux-support comes from universe now, which should be enabled
+sudo pacman-key --populate archlinux #only thing that was done the first go around was this line
+[[ $USEARCHREPOS = 'TRUE' ]] && echo "[extra]\nInclude = /etc/pacman.d/mirrorlist-arch\n\n[community]\nInclude=/etc/pacman.d/mirrorlist-arch\n\n[multilib]\nInclude=/etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf
+sudo chmod 644 /etc/pacman.conf
 
 #install nvidia drivers for NV110/GMXXX series or higher (checked via nvidia driver download page)
 # proprietary drivers, using for xorg. I chose proprietary by default b/c the speed diff b/t this and nouveau is too significant.
 # assuming usage of default kernel
-if [ $CARDTYPE == "NVIDIA" ] then
+if [ $CARDTYPE = "NVIDIA" ] then
     #creating a "just in case" file as the wiki suggested making a 20-intel.conf over an xorg.conf (likely b/c pacman has an affinity for breaking an xorg.conf)
     sudo pacman -S --noconfirm xf86-video-intel nvidia nvidia-settings
     chmod 777 /etc/X11/xorg.conf.d/ && sudo touch "/etc/X11/xorg.conf.d/20-intel.conf"
@@ -75,11 +88,11 @@ if [ $CARDTYPE == "NVIDIA" ] then
 
     #sudo pacman -S lib32-nvidia-utils #32-bit app support (multilib req, should already be configured)
     sudo mkdir /etc/pacman.d/hooks
-    sudo chown $USER:$USER /etc/pacman.d/hooks #chmod 777 worked for me for all instances of this the first time
+    sudo chown $USER /etc/pacman.d/hooks #chmod 777 worked for me for all instances of this the first time
     touch /etc/pacman.d/hooks/nvidia.hook && echo "[Trigger]\nOperation=Install\nOperation=Remove\nType=Package\nTarget=nvidia\nTarget=linux\n[Action]\nDepends=mkinitcpio\nWhen=PostTransaction\nNeedsTargets\nExec=/bin/sh -c 'while read -r trg; do case \$trg in linux) exit 0; esac ; done; /usr/bin/mkinitcpio -P'" > /etc/pacman.d/hooks/nvidia.hook 
     #above logic avoids no upgrade of initramfs after upgrading nvidia drivers, also prevents mkinitcpio from running more than once
-    sudo chown $USER:$USER -r /etc/pacman.d/hooks 
-    sudo chown $USER:$USER -r /etc/X11
+    sudo chown $USER -r /etc/pacman.d/hooks 
+    sudo chown $USER -r /etc/X11
     sudo nvidia-xconfig 
 fi
 
@@ -87,9 +100,9 @@ fi
 #this is VERY SPECIFIC, this command syntax I use here assumes you have 2 monitors: one is HDMI connected via HDMI-1 and it is on the right (user facing) of
 #a monitor connected by DVI-I-1. For my setup, the resolution defaults to 1920x1080, though I believe --mode can configure it (See "Multihead" on Arch Wiki)
 #xrandr --output DVI-I-1 --auto --output HDMI-0 --auto --right-of DVI-I-1
-#change primary monitor via display settings GUI of xfce4 (click on applications on the left of the top panel and find "Settings Manager" or "Display")
+#change primary monitor via display settings GUI of xfce (click on applications on the left of the top panel and find "Settings Manager" or "Display")
 cd $HOME
-[[ $USEXFCE4 == "TRUE" ]] && echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx' >> .bash_profile
+[[ $USEXFCE4 = "TRUE" ]] && echo '[[ ! $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx' >> .bash_profile
 [[ -f .asoundrc ]] && rm .asoundrc # its not supposed to exist
 #copy settings from the system's configuration into a user-specific config (changes made in the file inside home (~) will only affect the current user and are not system wide
 mkdir .pulse
@@ -125,9 +138,10 @@ sudo chmod 777 /etc/hosts && curl https://github.com/InquireWithin/resources/tre
 sudo pacman --noconfirm -R $(pacman -Qtdq)
 
 #launch the DE
-exec startx
-
-
+[[ $USEXFCE4 = 'TRUE' ]] && startxfce4 > /dev/null 2>&1 || exec startx > /dev/null 2>&1 || xinit > /dev/null 2>&1
+echo "If you're seeing this, there was one or more errors in attempting to start the X server."
+sleep 2
+exit 1
 
 
 
